55f0302d96008cf96456fad1fbd4e791d410b09a
choidslab <choids.lab@gmail.com>
2020-05-03 23:46:45 +0900

    ch4 소스코드

---
+++ ch4/vsearch2.py
@@ -0,0 +1,14 @@
+def search4vowels(phrase: str) -> set:  # Function Annotation 함수의 인자 유형 또는 리턴 유형을 문서화 하는 기능
+    """Return any vowels found in a supplied phrase."""
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
+
+
+def search4letters(phrase: str, letters: str='aeiou') -> set:
+    """Return a set of the 'letters' found in 'phrases'."""
+    return set(letters).intersection(set(phrase))
+
+
+print(search4vowels(input('Provide a word to search for vowels: ')))
+print(search4letters(input('Provide a word to search for vowels: '), 'test string'))
+print(search4letters(input('Provide a word to search for vowels: ')))
