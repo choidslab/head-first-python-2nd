2c5986defe3a675516d0adc258951f4e59a35f95
choidslab <choids.lab@gmail.com>
2020-05-04 11:25:01 +0900

    ch4 소스코드

---
+++ ch4/setup.py
@@ -0,0 +1,13 @@
+""" module 생성 및 배포 예제 코드(setuptools, setup.py, readme.txt"""
+
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='HF Python 2e',
+    author_email='hfpy2e@gmail.com',
+    url='headfirstlabs.com',
+    py_moudles=['vsearch'],
+)
