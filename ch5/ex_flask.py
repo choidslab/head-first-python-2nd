68ef8fbd45c81d7dd1f225255dec8cb7b90fa8b8
choidslab <choids.lab@gmail.com>
2020-05-05 22:29:31 +0900

    ex_flask.py 수정

--- ch5/ex_flask.py
+++ ch5/ex_flask.py
@@ -1,7 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/')  # decorator,route() 파라미터 값 = URL을 의미
 def hello_world():
     return 'Hello World from Flask!'
 
@@ -9,4 +11,5 @@ def hello_world():
 def intro_dslab():
     return '안녕하세요. DSLab에 오신 것을 환영합니다. :)'
 
-app.run()  # flask web server 실행
+
+app.run()  # flask web serve
