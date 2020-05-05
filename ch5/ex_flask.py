ffd3183b3cbe01cf22228d371e8bc3b30cc2c3a5
choidslab <choids.lab@gmail.com>
2020-05-05 22:08:38 +0900

    ch5 flask 샘플코드

--- ch5/ex_flask.py
+++ ch5/ex_flask.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Flask!'
 
+@app.route('/dslab')
+def intro_dslab():
+    return '안녕하세요. DSLab에 오신 것을 환영합니다. :)'
 
 app.run()  # flask web server 실행
